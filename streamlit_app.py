3a0b368e5a99e5d6d37c662f76f1359f69a5b7e0
Osama Ata <370095+osama-ata@users.noreply.github.com>
2024-10-15 12:54:46 +0000

    ..

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+from openpyxl import load_workbook
 
-st.title("🎈 My new app")
+worksheet_name = 'Action_Plan'
+Action_Plan_table_name = 'Action_Plan_Table'
+
+st.title("Action Plan Validator and Compiler")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This app was deveopped by [Osama Ata](https://osamata.com/)."
 )
+
+
+uploaded_files = st.file_uploader("Choose a file(s)",
+                                 type=['xlsx'],
+                                 accept_multiple_files=True)
+
+
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        # Process each uploaded file
+        wb = load_workbook(uploaded_file)
+        # Access the first sheet by default (adjust sheet name if needed)
+        ws = wb[worksheet_name]
+        Action_Plan_table = ws.tables[Action_Plan_table_name]
+        Action_Plan_table_range = Action_Plan_table.ref
+        st.success(f"{uploaded_file.name} has Action_Plan_table in {Action_Plan_table_range}")
